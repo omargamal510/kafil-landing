fb71eb2b6dba5835c517c1842b9e4c6b558175a0
omargamal510 <34138436+omargamal510@users.noreply.github.com>
2024-03-13 16:08:28 +0200

    [INIT] The project

---
+++ js/script.js
@@ -0,0 +1,69 @@
+
+
+/*Global variables*/
+
+/* Global variables */
+
+// console.log(navChilds);
+
+/* Start Navbar */
+const navSvgs = Array.from(document.querySelectorAll('.nav-anchor'));
+
+
+function navSvgsActive() {
+
+
+    navSvgs.forEach((svg, index) => {
+        svg.onmouseover = function () {
+            this.setAttribute('fill', '#1DBF73');
+            this.style.color = "#1DBF73";
+            this.classList.remove('nav-inactive')
+            setChildFill(this, '#1DBF73');
+        }
+
+        svg.onmouseleave = function () {
+            this.setAttribute('fill', '#C3C5C8');
+            this.classList.add('nav-inactive')
+            setChildFill(this, '#C3C5C8');
+        }
+
+    })
+}
+function setChildFill(element, color) {
+    const childElements = element.querySelectorAll('*');
+    childElements.forEach(child => {
+        child.setAttribute('fill', color);
+
+    });
+}
+navSvgsActive();
+
+// Prototype for real time counter
+
+let notifCount = 1;
+let intervalVar;
+let notifCountEle = Array.from(document.querySelectorAll('.notif-count'));
+function notifCounter() {
+    intervalId = setInterval(() => {
+        notifCountEle.forEach((ele, index) => {
+            if (notifCount >= 20) {
+                clearInterval(intervalId);
+                ele.innerHTML = `${notifCount}+`; // Clear the interval when count reaches 20
+            } else {
+                notifCount++;
+                ele.innerHTML = notifCount;
+            }
+
+            ele.parentElement.addEventListener('click', () => {
+                clearInterval(intervalId);
+                ele.innerHTML = '';
+            })
+        })
+    }, 1500);
+}
+notifCounter();
+
+
+
+
+/* End Navbar */
